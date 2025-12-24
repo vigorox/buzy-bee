6c5045a0df5edfabb811e8997d6bcb678e649a2a
E14\vigor <vigorox@gmail.com>
2025-12-24 09:27:14 +0800

    fix: use native confirm dialog in update prompt to avoid issues

--- public/version.js
+++ public/version.js
@@ -1,6 +1,6 @@
 // 统一版本管理配置文件
 // 只需修改这一个地方，所有文件会自动使用此版本号
-const APP_VERSION = '1.2.2';
+const APP_VERSION = '1.2.3';
 
 // 如果在 Service Worker 中，导出给 SW 使用
 if (typeof self !== 'undefined' && self instanceof ServiceWorkerGlobalScope) {
